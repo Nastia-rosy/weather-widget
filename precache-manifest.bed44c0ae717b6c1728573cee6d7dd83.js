1170cd2dc31fd9f55c9878b14616030f4e432492
Nastia-rosy <nastiiarosy@gmail.com>
2020-09-28 11:45:55 +0300

    Updates

---
+++ precache-manifest.bed44c0ae717b6c1728573cee6d7dd83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aead11b587fc0878d029591c732cd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43c8aa0aef86d2736f8",
+    "url": "/static/js/2.16737267.chunk.js"
+  },
+  {
+    "revision": "b0adff54d3835f1fa9008e2dcd8b3bfb",
+    "url": "/static/js/2.16737267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01df13229068ab6173c",
+    "url": "/static/js/main.2e910b5b.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  },
+  {
+    "revision": "96c42b3605ac9f5d8b7034919a11caf3",
+    "url": "/static/media/weather-background.96c42b36.jpg"
+  }
+]);
